85cb3f6c522b17ccf20d5b8a1a4d4086ea8f8607
PotlaETU <anthony156gamer@gmail.com>
2023-10-28 18:28:18 +0200

    Module modifié

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.potla.firstapp.leagueroleavis {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.desktop;
+
+
     opens com.potla.firstapp.leagueroleavis to javafx.fxml;
     exports com.potla.firstapp.leagueroleavis;
 }
