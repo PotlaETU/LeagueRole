7931dd49041b5cf434b11b45c8e5a6a2e3f07904
PotlaETU <anthony156gamer@gmail.com>
2023-10-28 18:28:00 +0200

    :zap: Utilisation de Map pour faciliter le traitement des images des rôles

--- src/main/java/com/potla/firstapp/leagueroleavis/AvisController.java
+++ src/main/java/com/potla/firstapp/leagueroleavis/AvisController.java
@@ -1,15 +1,13 @@
 package com.potla.firstapp.leagueroleavis;
 
-import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.util.Objects;
+import java.util.*;
 
 public class AvisController {
 
@@ -17,44 +15,45 @@ public class AvisController {
     private ChoiceBox<Role> choice;
 
     @FXML
-    private Button enter;
-
-    @FXML
-    private Button restart;
-
-    @FXML
     private ImageView image;
 
     @FXML
     private Label text;
 
+    private Map<String, String> rolesimages = new HashMap<>();
+
+    private String[] nomsFichiers = {"fakeradcmid.jpg", "ornn_portrait.jpg", "leesin.jpg", "adc.jpeg", "lux.jpg"};
+
+    private String[] place = {"mid","top","jgl", "adc", "sup"};
+
     @FXML
     void onEnter(ActionEvent event) {
         choice.setDisable(true);
         switch (choice.getValue().getCode()){
-            case "top"-> {Image ornn = new Image(Objects.requireNonNull(getClass().getResourceAsStream("ornn_portrait.jpg")));
+            case "top"-> {
+                Image ornn = new Image(Objects.requireNonNull(getClass().getResourceAsStream(rolesimages.get("top"))));
                 image.setImage(ornn);
                 text.setText("Toi t'es le giga chad de la team, tu solo lane toute la game, t'es le sigma originel");}
             case "jgl"-> {
-                Image leesin = new Image(Objects.requireNonNull(getClass().getResourceAsStream("leesin.jpg")));
+                Image leesin = new Image(Objects.requireNonNull(getClass().getResourceAsStream(rolesimages.get("jgl"))));
                 image.setImage(leesin);
                 text.setText("Et oui, tu portes sur tes épaules tout le malheur de la faille... T'as du courage");}
             case "mid"->{
-                Image midlane = new Image(Objects.requireNonNull(getClass().getResourceAsStream("fakeradcmid.jpg")));
+                Image midlane = new Image(Objects.requireNonNull(getClass().getResourceAsStream(rolesimages.get("mid"))));
                 image.setImage(midlane);
-                text.setText("Et oui, on veut tous ressembler au GOAT mais c'est pas en jouant des adc mid que tu vas lui ressembler...");}
+                text.setText("Et oui, on veut tous ressembler au GOAT. Alors Showmaker, Faker ou Chovy ? (KC Saken)");}
             case "adc"->{
-                Image adc = new Image(Objects.requireNonNull(getClass().getResourceAsStream("adc.jpeg")));
+                Image adc = new Image(Objects.requireNonNull(getClass().getResourceAsStream(rolesimages.get("adc"))));
                 image.setImage(adc);
                 text.setText("Rôle de pleurnichard qui insulte Riot quand ils ne peuvent pas os... Mais en vrai jgl diff pas vrai ?");}
 
             case "sup" ->{
-                Image sup = new Image(Objects.requireNonNull(getClass().getResourceAsStream("lux.jpg")));
+                Image sup = new Image(Objects.requireNonNull(getClass().getResourceAsStream(rolesimages.get("sup"))));
                 image.setImage(sup);
                 text.setText("Toi t'aimes les skins utlimes heinnnn... Essaye de ne pas ks l'adc ça serait cool...");}
 
             default -> throw new IllegalStateException("Unexpected value: " + choice.getValue().getCode());
-        }};
+        }}
     @FXML
     void onRestart(ActionEvent event){
         choice.setDisable(false);
@@ -68,6 +67,9 @@ public class AvisController {
         choice.getItems().add(new Role("mid","MidLane"));
         choice.getItems().add(new Role("adc","Attack Damage Carry"));
         choice.getItems().add(new Role("sup","Support"));
+        for(int i=0;i<place.length;i++){
+            rolesimages.put(place[i],nomsFichiers[i]);
+        }
     }
 
 }
